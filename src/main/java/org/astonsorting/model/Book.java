71d4ff4e4a1f8529ee7d95dad07c116b7627bb58
smallmind-creator <edwardbishop1@proton.me>
2025-10-29 00:28:26 +0400

    Переопределены equals() и hashCode()

--- src/main/java/org/astonsorting/model/Book.java
+++ src/main/java/org/astonsorting/model/Book.java
@@ -1,4 +1,5 @@
 package org.astonsorting.model;
+import java.util.Objects;
 
 public class Book implements Comparable<Book> {
     private final String title;
@@ -61,5 +62,20 @@ public class Book implements Comparable<Book> {
         return ("Название: " + " [" + this.title + "], " +
                 "Автор: " + " [" + this.author + "], " +
                 "Год: " + " [" + this.publicationYear + "]");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publicationYear == book.publicationYear &&
+                Objects.equals(title, book.title) &&
+                Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, publicationYear);
     }
 }
