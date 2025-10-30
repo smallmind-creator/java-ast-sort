6d6778ff234034a2aefa7a0b0aec5749f1828c3d
smallmind-creator <edwardbishop1@proton.me>
2025-10-30 13:36:52 +0400

    Исправлена ошибка использования оператора сравнения

--- src/main/java/org/astonsorting/test/BuilderTest.java
+++ src/main/java/org/astonsorting/test/BuilderTest.java
@@ -2,12 +2,14 @@ package org.astonsorting.test;
 
 import org.astonsorting.model.Book;
 
+import java.util.Objects;
+
 public class BuilderTest {
     public static void main(String[] args) {
 
         Book input = new Book.Builder().setTitle("A").setAuthor("B").setPublicationYear(2001).build();
 
-        if(input.getTitle() == "A" && input.getAuthor() == "B" && input.getPublicationYear() == 2001)
+        if(Objects.equals(input.getTitle(), "A") && Objects.equals(input.getAuthor(), "B") && input.getPublicationYear() == 2001)
             System.out.println("SUCCESS");
         else
             System.out.println("FAILURE");
